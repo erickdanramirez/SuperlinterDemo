ec05f4fa3b0a9396c1955e9ebb0fa1cf5f94634f
Erick Daniel Ramirez Martinez <107895714+erickdanramirez@users.noreply.github.com>
2022-06-28 14:54:02 -0500

    Update HelloController.java

--- src/main/java/com/hello/hello/HelloController.java
+++ src/main/java/com/hello/hello/HelloController.java
@@ -8,21 +8,48 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+* Hello Controller class 
+*/ 
 @RestController
 public class HelloController {
 
-    String greeting = "Hello World!";
-
+    private string greeting;    
+    public static final int FINALAVALUE = 5;
+    
+    /**
+    * Get the geeting variable        
+    */    
+    public String getGreeting() {    
+        return color;
+    }
+    
+    /**
+    * Sets the geeting variable
+    *
+    * @param c  the text of the greeting
+    */ 
+    public void setGreeting(String c) {
+        this.color = c;
+    }
+    
+    /**
+    * Index method controller that receives all the requests
+    */  
     @RequestMapping("/")
     public String index() {
-        return greeting;
+        setGreeting("Hello World!");
+        return getGreeting();
     }
-
+    
+    /**
+    * api method controller that receives the api routed requests
+    */ 
     @RequestMapping("/api")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Map<String, String> api() {
         HashMap<String, String> map = new HashMap<>();
-        int a = 5;
+        int a = FINALAVALUE;
         map.put("greeting", greeting);
         return map;
     }
